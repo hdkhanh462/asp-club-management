7c281d518e0ae5067cc4a38d79a52de72da1f76e
3vil3y3 <101619460+hdkhank462@users.noreply.github.com>
2025-02-07 16:14:37 +0700

    complete: FILTER & CREATE Events UI

--- IctuTaekwondo.Shared/Responses/Event/EventResponse.cs
+++ IctuTaekwondo.Shared/Responses/Event/EventResponse.cs
@@ -5,24 +5,13 @@ namespace IctuTaekwondo.Shared.Responses.Event
     public class EventResponse
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime? EndDate { get; set; }
-
         public string Location { get; set; }
-
         public string? Description { get; set; }
-
         public long? Fee { get; set; }
-
         public int? MaxParticipants { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime? CreatedAt { get; set; }
     }
 }
